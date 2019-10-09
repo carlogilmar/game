7baae0ec4d1c7808052182c142745122b020fa30
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-08 20:20:42 -0500

    #4 Implement funcs in guardian module for get user in session

--- lib/coca_cola_game/auth/guardian.ex
+++ lib/coca_cola_game/auth/guardian.ex
@@ -1,14 +1,24 @@
 defmodule CocaColaGame.Guardian do
   @moduledoc false
   use Guardian, otp_app: :coca_cola_game
+	alias CocaColaGame.UserOperator
 
-  def subject_for_token(user, _claims) do
-    {:ok, 0}
+  def subject_for_token(user, claims) do
+		validate(user)
   end
+
+	def validate({:ok, session, claims}) do
+		user_id = claims["sub"]
+		{:ok, user_id}
+	end
+
+	def validate(user) do
+		{:ok, user.id}
+	end
 
   def resource_from_claims(claims) do
     id = claims["sub"]
-    resource = %{username: ""}
-    {:ok,  resource}
+    user = CocaColaGame.UserOperator.get_by_id(id)
+    {:ok,  user}
   end
 end
