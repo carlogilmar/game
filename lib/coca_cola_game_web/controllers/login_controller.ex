db73ec3bb5275f0ef792649dae6ec1be63e924c6
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-08 20:21:21 -0500

    #4 Implement login in controller

--- lib/coca_cola_game_web/controllers/login_controller.ex
+++ lib/coca_cola_game_web/controllers/login_controller.ex
@@ -1,7 +1,40 @@
 defmodule CocaColaGameWeb.LoginController do
   use CocaColaGameWeb, :controller
+  alias CocaColaGame.Session
 
-  def index(conn, _params) do
-    render(conn, "index.html")
+  def index(conn, params) do
+    form_params = Map.has_key?(params, "_csrf_token")
+    validate_params.({form_params, conn, params})
   end
+
+  def login(conn, params) do
+    form_params = Map.has_key?(params, "_csrf_token")
+    validate_params.({form_params, conn, params})
+  end
+
+  defp validate_params do
+    fn
+      {false, conn, _params} ->
+        render(conn, "index.html", error: "")
+      {true, conn, params} ->
+        session = Session.auth_user(params["username"], params["password"])
+        login_reply(session, conn)
+    end
+  end
+
+  defp login_reply({:error, error}, conn) do
+    conn |> render("index.html", error: error)
+  end
+
+  defp login_reply({:ok, user}, conn) do
+    session = CocaColaGame.Guardian.encode_and_sign(user)
+    CocaColaGame.Guardian.Plug.sign_in(conn, session) |> redirect(to: "/home")
+  end
+
+  def logout(conn, _) do
+    conn
+    |> CocaColaGame.Guardian.Plug.sign_out()
+    |> redirect(to: "/login")
+  end
+
 end
