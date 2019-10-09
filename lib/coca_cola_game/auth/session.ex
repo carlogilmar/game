1d89d420318afa0c90f26e412d217486f4ea8cb8
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-08 19:34:19 -0500

    #4 Adding session for get a user from login

---
+++ lib/coca_cola_game/auth/session.ex
@@ -0,0 +1,22 @@
+defmodule CocaColaGame.Session do
+  alias CocaColaGame.UserOperator
+  @user_not_found "Usuario no encontrado"
+  @wrong_password "Password inválida"
+
+  def auth_user(user, password) do
+    user
+    |> UserOperator.get_by_username()
+    |> ensure_password(password)
+  end
+
+  def ensure_password(nil, _password), do: {:error, @user_not_found}
+  def ensure_password(user, password) do
+    password
+    |> Argon2.verify_pass(user.password)
+    |> auth_password_hash(user)
+  end
+
+  def auth_password_hash(true, user), do: {:ok, user}
+  def auth_password_hash(false, _user), do: {:error, @wrong_password}
+
+end
